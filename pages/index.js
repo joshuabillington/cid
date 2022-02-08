b5f846fc31868b0fd32675a29eb19e28c65c58fa
joshuabillington <48261725+joshuabillington@users.noreply.github.com>
2022-02-08 08:46:54 -0500

    Update index.js
    text changes

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
     <div>
       <Head>
           <title> Carswell Intelligent Designs </title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
